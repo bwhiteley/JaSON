72604650299248a3a9c2408b9ddd1eb9db109c0d
Bart Whiteley <@bwhiteley>
2015-10-07 23:17:39 -0600

    Initial Commit

---
+++ JaSON/JaSON.h
@@ -0,0 +1,19 @@
+//
+//  JaSON.h
+//  JaSON
+//
+//  Created by J. B. Whiteley on 10/7/15.
+//  Copyright © 2015 SwiftBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JaSON.
+FOUNDATION_EXPORT double JaSONVersionNumber;
+
+//! Project version string for JaSON.
+FOUNDATION_EXPORT const unsigned char JaSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JaSON/PublicHeader.h>
+
+
